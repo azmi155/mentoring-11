ad216c4e6d526290e7eed9383c6f9be1866ee47d
Muhammad-JK <58422190+Muhammad-JK@users.noreply.github.com>
2019-12-09 16:23:06 +0700

    KTP 2030

---
+++ Muhammad Audi Pramudia/Muhammad Audi Pramudia_Arrylist.java
@@ -0,0 +1,48 @@
+
+package arrykelasa;
+import java.util.ArrayList;
+public class TugasMentoring1 {
+    public static void main(String[] args) {
+        ArrayList audi = new ArrayList();
+        audi.add("5201010107000277");
+        audi.add("Muhammad Audi Pramudia");
+        audi.add("Mataram, 07 september 2000");
+        audi.add("Laki Laki");
+        audi.add("Perigi, Gerung Selatan");
+        audi.add("Islam");
+        audi.add("Jomblo");
+        audi.add("WNI");
+        
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("|          PROVINSI NUSA TENGGARA BARAT             |");
+        System.out.println("|             KABUPATEN LOMBOK BARAT                |");
+        System.out.println("|                                                   |");
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  NIK             : "+audi.get(0)+"");
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Nama            : "+audi.get(1));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  TTL             : "+audi.get(2));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Kelamain        : "+audi.get(3));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Alamat          : "+audi.get(4));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Agama           : "+audi.get(5));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Status          : "+audi.get(6));
+        System.out.println("----------------------------------------------------");
+        System.out.println("|                                                   |");
+        System.out.println("  Kewarganegaraan : "+audi.get(7));
+        System.out.println("----------------------------------------------------");
+    }
+    
+}
